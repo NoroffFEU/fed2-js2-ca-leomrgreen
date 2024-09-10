a65e6b1433850005a5fd3a29346fe05037f803e7
Leo Jonsson <leojon00003@stud.noroff.no>
2024-09-10 12:40:07 +0200

    changed style on navbar, changed noroff-logo

--- src/js/utilities/nav.js
+++ src/js/utilities/nav.js
@@ -5,7 +5,7 @@ export default function navMenu() {
   const profileTag = document.getElementById("profile");
 
   if (profileTag) {
-    profileTag.textContent = userName.name;
+    profileTag.textContent = userName.name.toUpperCase();
   } else {
     return null;
   }
