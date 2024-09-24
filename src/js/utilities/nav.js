b6c192983306f308ba97e5fec4b5dd2414edb076
Leo Jonsson <leojon00003@stud.noroff.no>
2024-09-24 09:28:32 +0200

    added handleOutSide functionality to pop up

--- src/js/utilities/nav.js
+++ src/js/utilities/nav.js
@@ -5,6 +5,17 @@ export default function setDropDownListener() {
   if (profileIcon) {
     profileIcon.addEventListener("click", () => {
       dropdown.classList.toggle("active");
+
+      // handle outside click function
+      if (dropdown.classList.contains("active")) {
+        document.body.addEventListener("click", (e) => {
+          if (!profileIcon.contains(e.target)) {
+            dropdown.classList.remove("active");
+          }
+        });
+      } else {
+        return null;
+      }
     });
   } else {
     return null;
