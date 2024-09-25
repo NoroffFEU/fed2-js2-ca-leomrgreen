c358f8956172fa9ef096298fa1678be13523fada
Leo Jonsson <146437864+leomrgreen@users.noreply.github.com>
2024-09-25 15:42:01 +0200

    added followbtn logic on popup following/followers

--- src/js/utilities/popUp.js
+++ src/js/utilities/popUp.js
@@ -1,13 +1,15 @@
 import { authGuard } from "./authGuard";
 import * as storage from "./storage";
+import ProfileAPI from "../api/profile";
 
+const api = new ProfileAPI();
 const user = storage.load("user");
 
 authGuard();
-
 const loggedInUser = user.name;
 
-export default function popUp(users) {
+export default async function popUp(users) {
+  const myProfile = await api.profile.read(loggedInUser);
   const popUp = document.querySelector(".popUp");
   const dropdown = document.createElement("div");
   dropdown.className = "user-dropdown";
@@ -34,13 +36,44 @@ export default function popUp(users) {
       const username = document.createElement("span");
       username.textContent = `@${user.name}`;
       username.addEventListener("click", () => {
-        if (user.name != loggedInUser) {
+        if (user.name !== loggedInUser) {
           window.location.href = `/user/?id=${user.name}`;
         } else {
           window.location.href = "/profile/";
         }
       });
-      flexBox.append(avatar, username);
+
+      // Create follow/unfollow button
+      const followBtn = document.createElement("button");
+
+      // Check if the logged-in user is following the current user
+      const isFollowing = myProfile.data.following?.some(
+        (follower) => follower.name === user.name // Change this to check against the current user
+      );
+
+      console.log("Following users:", myProfile.data.following);
+      console.log(`Is ${loggedInUser} following ${user.name}? ${isFollowing}`);
+
+      if (!isFollowing) {
+        followBtn.className = "primary";
+        followBtn.textContent = "Follow +";
+        followBtn.addEventListener("click", async () => {
+          await api.profile.follow(user.name);
+          window.location.reload();
+        });
+      } else {
+        followBtn.className = "following";
+        followBtn.textContent = "Following";
+        followBtn.addEventListener("click", async () => {
+          await api.profile.unfollow(user.name);
+          window.location.reload();
+        });
+      }
+      if (user.name === loggedInUser) {
+        flexBox.append(avatar, username);
+      } else {
+        flexBox.append(avatar, username, followBtn);
+      }
       dropdown.append(flexBox);
     });
   }
